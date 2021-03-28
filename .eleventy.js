4f34d6c55d4d8f9d268999104b544bfe84ae6ac1
Jonathan Yeong <jonathanyeong@gmail.com>
2021-03-28 11:45:25 -0700

    Update readme and eleventyjs

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,5 @@
 module.exports = config => {
   return {
-    templateFormats: [
-      "md",
-      "njk",
-      "html"
-    ],
-    markdownTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
     dir: {
       input: 'src',
       output: 'dist'
---
+++ README.md
@@ -0,0 +1,10 @@
+# Eleventy + Tailwind JIT
+
+This is an eleventy site that includes Tailwind JIT.
+
+## Getting started
+
+```
+npm install
+npm run dev
+```
